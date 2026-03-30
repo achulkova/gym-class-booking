e887c9b80b1bc494822467a82cdb7ccfd6b1ec5c
Anna Chulkova <anna.chulkova.java25@edu.edugrade.se>
2026-03-30 03:30:12 +0200

    feat: add User entity with unique username and role for JWT authentication

---
+++ src/main/java/se/edugrade/java25/enterprise/gym/model/User.java
@@ -0,0 +1,35 @@
+package se.edugrade.java25.enterprise.gym.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
