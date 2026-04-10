ba8c00da5daff89afa823d1769f0738fc0930155
Anna Chulkova <anna.chulkova.java25@edu.edugrade.se>
2026-04-10 03:12:35 +0200

    feat: implement custom user details service for authentication

---
+++ src/main/java/se/edugrade/java25/enterprise/gym/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package se.edugrade.java25.enterprise.gym.security;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import se.edugrade.java25.enterprise.gym.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
